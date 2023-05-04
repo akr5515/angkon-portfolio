aaaa2cd7f5b61279c85577dedbe5f74c87309919
Angkon Kumar Roy <akrangkon5515@gmail.com>
2023-05-04 22:27:38 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Box, Avatar, Typography, IconButton } from "@mui/material";
+import AngkonImage from "./images/angkon.jpg";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          width: "80%",
+          margin: "40px auto",
+          gap: "30px",
+        }}
+      >
+        <Box>
+          <Avatar
+            src={AngkonImage}
+            alt="Angkon"
+            sx={{ height: "170px", width: "150px" }}
+          />
+        </Box>
+        <Box>
+          <Typography sx={{ fontSize: "24px" }}>
+            Hi, Iam Angkon Kumar Roy
+          </Typography>
+          <Typography>
+            Software engineer with nearly 2years of front-end developing
+            experience with ReactJS and strong communication skill in
+            English(both verbal and written). Focusing on reusable components,
+            pixel perfect designing, problem solving and optimizing performance
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <IconButton
+          onClick={() => window.open("https://github.com/akr5515", "_blank")}
+          // sx={{ height: "50px", width: "50px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <GitHubIcon style={{ height: "40px", width: "40px" }} />
+        </IconButton>
+        <IconButton
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/angkon-kumar-roy/",
+              "_blank"
+            )
+          }
+        >
+          <LinkedInIcon style={{ height: "40px", width: "40px" }} />
+        </IconButton>
+      </Box>
+    </Container>
   );
 }
 
